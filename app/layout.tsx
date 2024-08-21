94aee071fe5a2aad233d054b44ebb4a61bd4c68c
fierzahaikkal <fierzahf.gdscug@gmail.com>
2024-08-21 21:43:24 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import UIProvider from "@/components/NextUIProvider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+        <UIProvider>
+          <main className="min-h-screen flex flex-col items-center">
+            {children}
+          </main>
+        </UIProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { Button } from "@nextui-org/button";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -30,6 +31,7 @@ export default async function Index() {
 
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
+        <Button color="primary">Test Button</Button>
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
