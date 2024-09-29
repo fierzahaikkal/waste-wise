8a9085c28a595a472ec675b21673b6533738ed6c
fierzahaikkal <fierzahf.gdscug@gmail.com>
2024-09-29 19:46:20 +0700

    fix: edu hero description

--- app/education/_components/hero.tsx
+++ app/education/_components/hero.tsx
@@ -9,10 +9,7 @@ const EduHero = () => {
       </h1>
       <p className="mx-auto max-w-[30ch] text-center md:max-w-[70ch]">
         Mari Ketahui Lebih Lanjut Tentang Sampah Setiap hari, kita menghasilkan sampah yang bisa
-        berdampak buruk pada lingkungan jika tidak dikelola dengan baik. Dengan memahami jenis-jenis
-        sampah dan bagaimana cara mengolahnya, kita dapat berkontribusi untuk menjaga bumi tetap
-        bersih dan sehat. Yuk, pelajari bagaimana sampah bisa diolah, didaur ulang, dan dikelola
-        secara bijak untuk masa depan yang lebih hijau!
+        berdampak buruk pada lingkungan jika tidak dikelola dengan baik.
       </p>
       <div className="flex items-center gap-x-4">
         <Link href={"#waste-types"}>Baca selengkapnya</Link>
