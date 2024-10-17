f6d7486f8954d4d0ea1a50f8d8ac93b8b6fcc046
fierzahaikkal <fierzahf.gdscug@gmail.com>
2024-10-17 12:02:58 +0700

    fix: remove image url field on wasteform

--- app/(user)/user/dashboard/waste-bank/_components/waste-form.tsx
+++ app/(user)/user/dashboard/waste-bank/_components/waste-form.tsx
@@ -92,18 +92,6 @@ const WasteForm = (props: WasteFormProps) => {
         />
       </div>
 
-      <div>
-        <label htmlFor="img_url" className="block text-sm font-medium text-gray-700">
-          Image URL
-        </label>
-        <input
-          name="img_url"
-          id="img_url"
-          onChange={handleChange}
-          className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-highland-500 focus:outline-none focus:ring-highland-500 sm:text-sm"
-        />
-      </div>
-
       {/* Jenis Sampah ID */}
       <div>
         <label htmlFor="jenis_sampah_id" className="block text-sm font-medium text-gray-700">
