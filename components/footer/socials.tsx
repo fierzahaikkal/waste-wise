070c483719424f5e0198cc2ecd9ef96d524524d7
fierzahaikkal <fierzahf.gdscug@gmail.com>
2024-10-24 09:46:45 +0700

    feat: interactive social link

--- components/footer/socials.tsx
+++ components/footer/socials.tsx
@@ -1,17 +1,24 @@
-import { Instagram, Linkedin, Twitter } from "lucide-react";
+import { Instagram, Linkedin, MapPinned, Twitter } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-4">
-      <Link href="/" className="grid place-items-center rounded-full bg-white p-3 leading-none">
-        <Linkedin color="black" />
+      <Link
+        href="https://maps.app.goo.gl/efakfH7KtxFFGz15A?g_st=com.google.maps.preview.copy"
+        rel="noopener noreferrer"
+        target="_blank"
+        className="grid place-items-center rounded-full bg-white p-3 leading-none"
+      >
+        <MapPinned color="black" />
       </Link>
-      <Link href="/" className="grid place-items-center rounded-full bg-white p-3 leading-none">
-        <Twitter color="black" />
-      </Link>
-      <Link href="/" className="grid place-items-center rounded-full bg-white p-3 leading-none">
+      <Link
+        href="http://instagram.com/wastewise12"
+        rel="noopener noreferrer"
+        target="_blank"
+        className="grid place-items-center rounded-full bg-white p-3 leading-none"
+      >
         <Instagram color="black" />
       </Link>
     </div>
